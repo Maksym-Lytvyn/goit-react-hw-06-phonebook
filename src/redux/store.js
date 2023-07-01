08d7cbbc7cfbd038cf9f7937f22f7d852fe86a47
Максим Литвин / Maksym Lytvyn <104524765+Maksym-Lytvyn@users.noreply.github.com>
2023-07-01 19:12:28 +0300

    up

--- src/redux/store.js
+++ src/redux/store.js
@@ -6,13 +6,39 @@
 // export const store = createStore(rootReducer, enhancer);
 
 import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import {
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 // import rootReducer from "./rootReducer";
 import contactsReducer from './contactsSlice';
 
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: 'contacts',
+};
+
+const persistedReducer = persistReducer(persistConfig, contactsReducer);
+
 const store = configureStore({
   reducer: {
-    contacts: contactsReducer,
+    contacts: persistedReducer,
   },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
-export default store;
+const persistor = persistStore(store);
+
+export { store, persistor };
